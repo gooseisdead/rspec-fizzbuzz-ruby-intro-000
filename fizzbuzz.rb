5bdd47c17d345852e201ceb66daecb860e2d6ac2
Mike Smith <gooseisdead@mac.com>
2020-08-02 01:45:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 int = [1.upto(100)]
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    puts "Fizz"
-  elsif int % 5 == 0
-    puts "Buzz"
-  elsif int % 15 == 0
-    puts "FizzBuzz"
-  else int % 4 == 0
-    puts "nil"
+  if int % 3 == 0 do
+    puts "Fizz" end
+  elsif int % 5 == 0 do
+    puts "Buzz" ends
+  elsif int % 15 == 0 do
+    puts "FizzBuzz" end
+  else int % 4 == 0 do
+    puts "nil" end
   end
 end
