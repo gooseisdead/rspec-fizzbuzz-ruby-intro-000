1b9c9a7001375add79fd8e10f6a178b39481f50d
Mike Smith <gooseisdead@mac.com>
2020-08-02 01:31:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,7 @@
-def fizzbuzz(number)
-  return 'FizzBuzz' if is_divisible_by_fifteen?(number)
-  return 'Buzz' if is_divisible_by_five?(number)
-  return 'Fizz' if is_divisible_by_three?(number)
-  return 'nil' if is_not_divisible_by_three_or_five(number)
-end
-
-def is_divisible_by_three?(number)
-  is_divisible_by(number, 3)
-end
-
-def is_divisible_by_five?(number)
-  is_divisible_by(number, 5)
-end
-
-def is_divisible_by_fifteen?(number)
-  is_divisible_by(number, 15)
+def fizzbuzz(int)
+  return "Fizz" if int % 3 == 0
+  return "Buzz" if int % 5 == 0
+  return "FizzBuzz" if int % 15 == 0
+  return "nil" if int % 4 == 0
+ end
 end
