c00c5dec51e23af7ffdcdef72a1d430d21b19e5e
Mike Smith <gooseisdead@mac.com>
2020-08-02 02:13:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
     return "Fizz"
+  elsif int % 4 == 0
+    puts "nil"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 15 == 0
+  else int % 15 == 0
     puts "FizzBuzz"
-  else int % 4 == 0
-    puts "nil"
   end
 end
